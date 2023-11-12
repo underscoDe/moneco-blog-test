8cc6c35fe920ff36e22701d3a9f6849ffe5c62ba
Arsene Awounou <developer.awounou@gmail.com>
2023-11-12 08:17:30 +0100

    feat: Blog card component

--- src/components/Articles/Blog.tsx
+++ src/components/Articles/Blog.tsx
@@ -6,8 +6,39 @@ interface BlogCardProps {
   previewUrl: string;
   tags: string[];
   excerpt: string;
-  readingTime: number;
-  date: string;
+  createdAt: Date;
+  content: Array<{
+    type: string;
+    children: Array<{
+      type: string;
+      text: string;
+    }>;
+  }>;
+}
+
+interface ContentItem {
+  type: string;
+  children: Array<{
+    type: string;
+    text: string;
+  }>;
+}
+
+function calculateReadingTime(content: ContentItem[]): number {
+  const totalWords = content.reduce((acc, paragraph) => {
+    paragraph.children.forEach((child) => {
+      if (child.type === 'text' && child.text) {
+        acc += child.text.split(/\s+/).length;
+      }
+    });
+    return acc;
+  }, 0);
+
+  // Assuming an average reading speed of 200 words per minute
+  const wordsPerMinute = 200;
+  const readingTime = Math.ceil(totalWords / wordsPerMinute);
+
+  return readingTime;
 }
 
 function BlogCard({
@@ -15,21 +46,31 @@ function BlogCard({
   previewUrl,
   tags,
   excerpt,
-  readingTime,
-  date,
+  createdAt,
+  content,
 }: BlogCardProps) {
+  const formattedDateDay = new Intl.DateTimeFormat('en-US', {
+    month: 'short',
+    day: 'numeric',
+  }).format(createdAt);
+
+  const formattedDateTime = new Intl.DateTimeFormat('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+  }).format(createdAt);
+
   return (
     <article className='mb-16 max-w-sm'>
       <div className='relative'>
         <Image
-          width='246'
-          height='140'
+          width='1536'
+          height='864'
           src={previewUrl}
-          className='mb-4 w-full'
+          className='mb-4 h-[240px] w-full object-cover'
           alt='Image 1'
         />
         <span className='text-decoration-none font-outfit absolute left-[4%] top-[4%] rounded-full bg-[#007554] px-4 py-1 text-base text-white'>
-          {date} · 02:35
+          {formattedDateDay} · {formattedDateTime}
         </span>
       </div>
       <div className='flex flex-wrap gap-x-2'>
@@ -48,7 +89,7 @@ function BlogCard({
       </h3>
       <p className='mb-2 text-[14px] font-medium text-black'>{excerpt}</p>
       <span className='items-center text-[14px] font-medium text-[#007554]'>
-        {date} · {readingTime} min read
+        {formattedDateDay} · {calculateReadingTime(content)} min read
       </span>
     </article>
   );
